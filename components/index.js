2ef341f67834e67b0211528a73e9c74c16ed3bcf
leemotive <wstation@163.com>
2018-12-13 23:09:34 +0800

    更新编译脚本

--- components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Form } from './form';
-export { default as Table } from './table';
-export { default as Utils } from './utils';
--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,33 @@ const less = require('gulp-less');
 const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 const cssmin = require('gulp-cssmin');
+const merge = require('merge-stream');
 const dir = require('node-dir');
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 const webpack = require('webpack');
-const es = require('event-stream');
+const fse = require('fs-extra');
 
 gulp.task('less:es', () => {
-  const css = gulp.src('./components/**/index.less')
+  const css = gulp
+    .src('./components/**/index.less')
     .pipe(less())
     .pipe(gulp.dest('es'));
-  const origin = gulp.src('./components/**/*.less')
-    .pipe(gulp.dest('es'));
-
-  return es.concat(css, origin);
+  const origin = gulp.src('./components/**/*.less').pipe(gulp.dest('es'));
+  return merge(css, origin);
 });
 gulp.task('less:lib', () => {
-  const css = gulp.src('./components/**/index.less')
+  const css = gulp
+    .src('./components/**/index.less')
     .pipe(less())
     .pipe(gulp.dest('lib'));
-
-  const origin = gulp.src('./components/**/*.less')
-    .pipe(gulp.dest('lib'));
-
-  return es.concat(css, origin);
+  const origin = gulp.src('./components/**/*.less').pipe(gulp.dest('lib'));
+  return merge(css, origin);
 });
 gulp.task('less:dist', () => {
-  gulp.src('./components/index.less')
+  gulp
+    .src('./components/index.less')
     .pipe(less())
     .pipe(cssmin())
     .pipe(rename('torenia.min.css'))
@@ -40,13 +39,15 @@ gulp.task('less:dist', () => {
 gulp.task('less', ['less:es', 'less:lib', 'less:dist']);
 
 gulp.task('build:es', () => {
-  gulp.src(babelSrc)
+  gulp
+    .src(babelSrc)
     .pipe(babel(babelConfiguartion(false)))
     .pipe(gulp.dest('./es'));
 });
 
 gulp.task('build:lib', () => {
-  gulp.src(babelSrc)
+  gulp
+    .src(babelSrc)
     .pipe(babel(babelConfiguartion('commonjs')))
     .pipe(gulp.dest('./lib'));
 });
@@ -70,11 +71,11 @@ gulp.task('build:dist', () => {
               loader: 'babel-loader',
               options: {
                 ...babelConfiguartion(),
-              }
-            }
-          ]
-        }
-      ]
+              },
+            },
+          ],
+        },
+      ],
     },
     externals: {
       react: 'React',
@@ -82,38 +83,56 @@ gulp.task('build:dist', () => {
       moment: 'moment',
       'prop-types': 'PropTypes',
       antd: 'antd',
-    }
+    },
   });
   return new Promise(resolve => {
     compile.run(function(err, stats) {
-      console.log(stats.toString({colors: true}));
+      /* eslint-disable-next-line no-console */
+      console.log(stats.toString({ colors: true }));
       resolve();
     });
   });
-
 });
 
-gulp.task('build', ['build:es', 'build:lib', 'build:dist']);
+gulp.task('build', ['build:es', 'build:lib', 'build:dist'], () => {
+  const paths = dir.files('./components', 'dir', () => {}, {
+    recursive: false,
+    sync: true,
+    shortName: true,
+  });
 
+  paths.forEach(d => {
+    fse.ensureFile(path.resolve(__dirname, 'es', d, 'style/index.js'));
+    fse.ensureFile(path.resolve(__dirname, 'lib', d, 'style/index.js'));
+  });
+});
 
 gulp.task('entry:js', () => {
-  const paths = dir.files('./components', 'dir', () => { }, {
-    recursive: false, sync: true, shortName: true,
+  const paths = dir.files('./components', 'dir', () => {}, {
+    recursive: false,
+    sync: true,
+    shortName: true,
   });
-  const content = paths.map(name => {
-    const exportName = `${name.charAt(0).toUpperCase()}${name.slice(1)}`
-    return `export { default as ${exportName} } from './${name}';`
-  }).join('\n');
+  const content = paths
+    .map(name => {
+      const exportName = `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
+      return `export { default as ${exportName} } from './${name}';\n`;
+    })
+    .join('');
   fs.writeFileSync('./components/index.js', content, {
-    encoding: 'utf8'
+    encoding: 'utf8',
   });
 });
 
 gulp.task('entry:less', () => {
   const files = glob.sync('./*/style/index.less', { cwd: './components' });
-  fs.writeFileSync('./components/index.less', files.map(f => `@import '${f}';`).join('\n'), {
-    encoding: 'utf8'
-  });
+  fs.writeFileSync(
+    './components/index.less',
+    files.map(f => `@import '${f}';`).join('\n'),
+    {
+      encoding: 'utf8',
+    },
+  );
 });
 
 gulp.task('entry', ['entry:js', 'entry:less']);
@@ -122,13 +141,10 @@ const babelSrc = ['./components/**/*.js'];
 function babelConfiguartion(modules) {
   const option = { modules };
   return {
-    presets: [
-      ['@babel/preset-env', option],
-      '@babel/preset-react'
-    ],
+    presets: [['@babel/preset-env', option], '@babel/preset-react'],
     plugins: [
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-export-default-from',
-    ]
-  }
+    ],
+  };
 }
